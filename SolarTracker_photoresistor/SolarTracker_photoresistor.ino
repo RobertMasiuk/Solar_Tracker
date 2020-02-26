92a1b48fe52e1bfe690ee81f0ec0dd0b06f66a14
Robert Masiuk <robert@masiuk.pl>
2020-02-26 23:36:00 +0100

    box

--- SolarTracker_photoresistor/SolarTracker_photoresistor.ino
+++ SolarTracker_photoresistor/SolarTracker_photoresistor.ino
@@ -2,7 +2,7 @@
    Test Program
 */
 int photoresistor1 = A0;
-//int photoresistor2 = A1;
+int photoresistor2 = A1;
 int photoresistor3 = A2;
 int photoresistor4 = A3;
 
@@ -12,12 +12,20 @@ int sensorValuePhotoresistor2 = 0;
 int sensorValuePhotoresistor3 = 0;
 int sensorValuePhotoresistor4 = 0;
 
+int axisXPlus = 10;
+int axisXMinus = 11;
+
+int sensitiveness;
+
 int total = 0;
 
 
 void setup() {
 
   pinMode(optimum, OUTPUT);
+  pinMode(axisXPlus, OUTPUT);
+  pinMode(axisXMinus, OUTPUT);
+
   Serial.begin(9600);
 }
 
@@ -28,14 +36,51 @@ void loop() {
   sensorValuePhotoresistor3 = analogRead(photoresistor3);
   sensorValuePhotoresistor4 = analogRead(photoresistor4);
 
-  total = sensorValuePhotoresistor1 + sensorValuePhotoresistor2 + sensorValuePhotoresistor3 + sensorValuePhotoresistor4;
+  //  total = sensorValuePhotoresistor1 + sensorValuePhotoresistor2 + sensorValuePhotoresistor3 + sensorValuePhotoresistor4;
+  //
+  //  total = total / 4;
+  //  Serial.println(total);
+  //  if (sensorValuePhotoresistor1 >= 180) {
+  //    Serial.println("test");
+  //    digitalWrite(optimum, HIGH);
+  //  }
+  //  if (sensorValuePhotoresistor1 >= 180) {
+  //    Serial.println("test");
+  //    digitalWrite(optimum, HIGH);
+  //  }
+  //
 
-  total = total / 4;
-  Serial.println(total);
-  if (sensorValuePhotoresistor1 >= 180) {
-    Serial.println("test");
-    digitalWrite(optimum, HIGH);
+  // ONE AXIS PIN ANALOG 3&1
+  sensitiveness = 30;
+  if (sensorValuePhotoresistor2 >= 150 || sensorValuePhotoresistor4 >= 150)// CHECK "IF DAY, DO"
+  {
+    Serial.print(" TEST ");
+    
+    if (sensorValuePhotoresistor2 >= (sensorValuePhotoresistor4 + sensitiveness)) {
+      digitalWrite(axisXPlus, LOW);
+      digitalWrite(axisXMinus, HIGH);
+      digitalWrite(optimum, LOW);
+        delay(100);
+    }
+    else if(sensorValuePhotoresistor4 >= (sensorValuePhotoresistor2 + sensitiveness)) {
+      digitalWrite(axisXPlus, HIGH);
+      digitalWrite(axisXMinus, LOW);
+      digitalWrite(optimum, LOW);
+        delay(100);
+    }
+  
+  
+
   }
+
+     if((sensorValuePhotoresistor2 - sensitiveness) <= sensorValuePhotoresistor4 <= (sensorValuePhotoresistor2 + sensitiveness)) {
+      digitalWrite(axisXPlus, LOW);
+      digitalWrite(axisXMinus, LOW);
+      digitalWrite(optimum, HIGH);
+    }
+
+
+
   Serial.print(sensorValuePhotoresistor1);
   Serial.print(" | ");
   Serial.print(sensorValuePhotoresistor2);
